b8c8336ad5e8de507ec97649c0c62e76712786ff
AKvin <kevin.alvespinto@gmail.com>
2019-09-18 14:21:06 +0200

    ajout de la class Location

---
+++ src/app/shared/models/location.ts
@@ -0,0 +1,13 @@
+export class Location {
+
+  readonly idLocation: string;
+  startDate: number;
+  endDate: number;
+  startTime: number;
+  endTime: number;
+  readonly idUser: string;
+  readonly idVehicule: string;
+  state: string[];
+
+  constructor() {}
+}
