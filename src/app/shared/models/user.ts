c5ee0bd28bc46e18a548e053aebbae0c2eea5b53
AKvin <kevin.alvespinto@gmail.com>
2019-09-18 11:57:39 +0200

    génération de la class User

---
+++ src/app/shared/models/user.ts
@@ -0,0 +1,16 @@
+export class User {
+  nom: string;
+  prenom: string;
+  dateNaissance: Date;
+  numRue: number;
+  nomRue: string;
+  voie: string;
+  ville: string;
+  codePostal: number;
+  telephone: string;
+  mail: string;
+  password: string;
+  numPermis: string;
+
+  constructor() {}
+}
