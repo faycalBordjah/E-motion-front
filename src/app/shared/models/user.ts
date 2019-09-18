345bece86347d1db1df6791972b82457799bcf70
AKvin <kevin.alvespinto@gmail.com>
2019-09-18 14:30:27 +0200

    minor modif on User

--- src/app/shared/models/user.ts
+++ src/app/shared/models/user.ts
@@ -1,14 +1,14 @@
 export class User {
 
   readonly idUser: string;
-  name: string;
+  firstName: string;
   lastName: string;
   bearth: Date;
   num: number;
   street: string;
   lane: string;
   town: string;
-  codePostal: number;
+  zipCode: number;
   phone: string;
   email: string;
   password: string;
