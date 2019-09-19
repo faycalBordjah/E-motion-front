ce0c7e8d908b1754132539da7becc0739f8e7dad
AKvin <kevin.alvespinto@gmail.com>
2019-09-19 14:34:50 +0200

    minor modif on ProtectedModule

--- src/app/protected/protected.module.ts
+++ src/app/protected/protected.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { UserModule } from '../protected/user/user.module';
+import { LocationModule } from '../protected/location/location.module';
+import { FactureModule } from '../protected/facture/facture.module';
+import { VehiculeModule } from '../protected/vehicule/vehicule.module';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    UserModule,
+    LocationModule,
+    FactureModule,
+    VehiculeModule
   ]
 })
 export class ProtectedModule { }
