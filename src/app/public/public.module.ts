51c36d96df6a07ef28ca39812d28d77a50b9ac32
AKvin <kevin.alvespinto@gmail.com>
2019-09-19 14:36:47 +0200

    minor modif on PublicModule

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -2,11 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeModule } from './home/home.module';
 import { LoginModule } from './login/login.module';
-import { UserModule } from '../protected/user/user.module';
-import { LocationModule } from '../protected/location/location.module';
-import { FactureModule } from '../protected/facture/facture.module';
-import { VehiculeModule } from '../protected/vehicule/vehicule.module';
-
 
 
 @NgModule({
@@ -14,11 +9,7 @@ import { VehiculeModule } from '../protected/vehicule/vehicule.module';
   imports: [
     CommonModule,
     HomeModule,
-    LoginModule,
-    UserModule,
-    LocationModule,
-    FactureModule,
-    VehiculeModule
+    LoginModule
   ]
 })
 export class PublicModule { }
