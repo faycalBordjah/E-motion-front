02f5e5e77fc3062196d076df04c4d09ff28aee3b
Fayçal BORDJAH <fbordjah@linagora.com>
2019-10-08 22:47:06 +0200

    Improve the behavior of the registration form (add some alters on empty fields)

--- src/app/public/register/register/register.component.ts
+++ src/app/public/register/register/register.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, FormsModule, NgForm, Validators} from '@angular/forms';
 import {AuthService} from '../../../shared/services/auth.service';
 import {UserService} from '../../../shared/services/user.service';
+import {AlertService} from '../../../shared/services/alert.service';
+import {Router} from '@angular/router';
+import {first} from 'rxjs/operators';
 
 @Component({
     selector: 'app-register',
@@ -15,14 +18,16 @@ export class RegisterComponent implements OnInit {
 
     constructor(private formBuilder: FormBuilder,
                 private authService: AuthService,
-                private userService: UserService) {
+                private userService: UserService,
+                private alertService: AlertService,
+                private router: Router) {
     }
 
     ngOnInit() {
         this.registerForm = this.formBuilder.group({
             firstName: ['', Validators.required],
             lastName: ['', Validators.required],
-            mail: ['', Validators.required],
+            mail: ['', Validators.required, Validators.email],
             password: ['', [Validators.required, Validators.minLength(6)]],
             birthDate: ['', Validators.required],
             phone: ['', Validators.required],
@@ -34,24 +39,27 @@ export class RegisterComponent implements OnInit {
         });
     }
 
+    get getFields() {
+        return this.registerForm.controls;
+    }
+
     onSubmit() {
         this.submitted = true;
-
         if (this.registerForm.invalid) {
             return;
         }
-
+        this.alertService.clear();
         this.loading = true;
-        this.userService.register(this.registerForm.value)
+        this.userService.register(this.registerForm.value).pipe(first())
             .subscribe(
-                /* data => {
+                data => {
                     this.alertService.success('Registration successful', true);
                     this.router.navigate(['/login']);
                 },
                 error => {
                     this.alertService.error(error);
                     this.loading = false;
-                } */);
+                });
     }
 
     register(form: FormGroup) {
