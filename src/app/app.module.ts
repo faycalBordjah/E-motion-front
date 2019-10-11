98c4d0c3d6fda4f7cf6322081a0bc1ac0a0cdd0c
AKvin <kevin.alvespinto@gmail.com>
2019-10-11 23:10:08 +0200

    add interceptor to providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
+import { TokenInterceptor } from './shared/helpers/interceptor';
+import { ErrorInterceptor } from './shared/helpers/error.interceptor';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [
