b45aca7de4afdc3c77aaf86621f346a1d9fbedbb
AKvin <kevin.alvespinto@gmail.com>
2019-10-21 12:48:39 +0200

    add model to générate class for decode jwt and minor modif and user model / create Adress - Role

---
+++ src/app/shared/models/user-authority.spec.ts
@@ -0,0 +1,7 @@
+import { UserAuthority } from './user-authority';
+
+describe('UserAuthority', () => {
+  it('should create an instance', () => {
+    expect(new UserAuthority()).toBeTruthy();
+  });
+});
