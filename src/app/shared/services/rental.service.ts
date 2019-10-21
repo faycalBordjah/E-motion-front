aa7674fa6d2e97a46c3174a0c44d482fb9ccd14f
AKvin <kevin.alvespinto@gmail.com>
2019-10-21 12:50:04 +0200

    Improvement of the services and add observable

--- src/app/shared/services/rental.service.ts
+++ src/app/shared/services/rental.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
-import { RentalModule } from 'src/app/protected/rental/rental.module';
 import { AppConstants } from './../constants/app.constants';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { Rental } from '../models/rental';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({
-  providedIn: RentalModule
+  providedIn: 'root'
 })
 export class RentalService {
 
   public apiLocation = AppConstants.api_location_url;
+
+  private readonly Rentals: BehaviorSubject<Rental[]>;
+  public readonly rentals$: Observable<Rental[]>;
+  private listRentals: Rental[];
 
   private headers = new HttpHeaders({
     'Access-Control-Allow-Origin': '*',
@@ -18,11 +23,32 @@ export class RentalService {
   });
 
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.Rentals = new BehaviorSubject<Rental[]>(this.listRentals);
+    this.rentals$ = this.Rentals.asObservable();
+   }
 
+  getAll(): Observable<any> {
+    return this.http.get<any>(this.apiLocation, {headers: this.headers})
+    .pipe(map(data => {
+      this.storeRentalsList(data.result);
+    }));
+  }
+  /*
   // user$ is id
   getAll(user$: number): Observable<any> {
     return this.http.get<any>(this.apiLocation + '/' + user$, {headers: this.headers});
+  }*/
+
+  storeRentalsList(rentals: Rental[]) {
+    console.log(rentals);
+    this.listRentals = rentals as Rental[];
+    this.Rentals.next(this.listRentals);
   }
+
+  public get currentRentalsValue(): Rental[] {
+    return this.Rentals.value;
+  }
+
 
 }
