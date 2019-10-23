ce99e7fdcf3a97b668edf38156159c81a084f592
AKvin <kevin.alvespinto@gmail.com>
2019-10-23 14:51:44 +0200

    improvement of manage user feature

--- src/app/protected/manage/manage-users/user-details/user-details.component.ts
+++ src/app/protected/manage/manage-users/user-details/user-details.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, TemplateRef } from '@angular/core';
 import { User } from 'src/app/shared/models/user';
 import { NgForm } from '@angular/forms';
 import { UserService } from 'src/app/shared/services/user.service';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-user-details',
@@ -13,15 +14,32 @@ export class UserDetailsComponent implements OnInit {
   @Input()
   user: User;
 
+  modalRef: BsModalRef;
+
+  config = {
+    keyboard: true
+  };
+
   @ViewChild('userUpdateForm', {static: false}) userUpdateForm: NgForm;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private modalService: BsModalService) { }
 
   ngOnInit() {
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, this.config);
   }
 
   updateUser(form: NgForm) {
     //this.vehicleService.update(form);
     console.log(form);
+  }
+
+  deleteUser(): void {
+    const userId = this.user.id.valueOf();
+    this.userService.deleteUser(userId).subscribe(data => {
+      console.log(data);
+    });
   }
 }
